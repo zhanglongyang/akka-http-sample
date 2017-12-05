d7f6993e63523d010620bbd743fe3f7ffb806633
Longyang Zhang <longyang.zhang@myob.com>
2017-12-06 09:10:32 +1100

    add hello-akka-http sample

---
+++ src/main/scala/example/WebServer.scala
@@ -0,0 +1,33 @@
+package example
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+object WebServer {
+  def main(args: Array[String]) = {
+    implicit val system = ActorSystem("akka-http-sample")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Hello Akka-http!</h1>"))
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 6060)
+
+    println(s"Server online at http://localhost:6060/\nPress RETURN to stop...")
+    StdIn.readLine()
+
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
+}
